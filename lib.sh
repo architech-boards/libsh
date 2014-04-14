3c655fadfb2d43ccab48c9bb8beadd4d134ae589
rspelta <pippo@architech.(none)>
2014-04-14 17:25:18 +0200

    added message_error function

--- lib.sh
+++ lib.sh
@@ -2,8 +2,12 @@
 # Script library
 
 
+readonly ERROR_INTERNET=1
+readonly ERROR_COMMAND=2
+
 #######################################################################################################################
 # gets form user the sudo password and exports it
+# param: none
 function get_sudo_password
 {
 	local ZENITY_INSTALLED
@@ -36,10 +40,35 @@ function get_sudo_password
 
 #######################################################################################################################
 # execute a command with sudo privileges
+# param: $1 string with the command to execute
 function do_sudo
 {
 	local CMD
 	CMD=$1
 	echo -e ${SUDO_PASSWORD} | sudo -S bash -c "${CMD}"
+}
+
+#######################################################################################################################
+# message error procedure
+# param: $1 error code (ERROR_INTERNET or ERROR_COMMAND)
+function message_error
+{
+	local ERROR_CODE
+	ERROR_CODE=$1
+	local ERROR_MESSAGE
+
+	case ${ERROR_CODE} in
+	${ERROR_INTERNET})
+	  ERROR_MESSAGE="Impossible to connect to internet, double check your Internet connection."
+	  ;;
+	${ERROR_COMMAND})
+	  ERROR_MESSAGE="Error in a command, please check log file."
+	  ;;
+	*)
+	  ERROR_MESSAGE="Unknown error, please check log file"
+	  ;;
+	esac
+
+	echo ${ERROR_MESSAGE}
 }
 
