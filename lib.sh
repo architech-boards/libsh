f205584e1f2484f0963344345ae4b40a7c1a9e5a
rspelta <pippo@architech.(none)>
2014-04-14 16:13:57 +0200

    fix local variables

--- lib.sh
+++ lib.sh
@@ -6,7 +6,10 @@
 # gets form user the sudo password and exports it
 function get_sudo_password
 {
-	local ZENITY_INSTALLED=`dpkg-query -l | grep zenity-common |& awk -F" " '{ print $1 }'`
+	local ZENITY_INSTALLED
+	local SUDO_PASSWORD2
+
+	ZENITY_INSTALLED=`dpkg-query -l | grep zenity-common |& awk -F" " '{ print $1 }'`
 
 	if [ "${USER}" != "architech" -o "${HOME}" != "/home/architech" ] 
 	then
@@ -35,6 +38,8 @@ function get_sudo_password
 # execute a command with sudo privileges
 function do_sudo
 {
+	local CMD
 	CMD=$1
 	echo -e ${SUDO_PASSWORD} | sudo -S bash -c "${CMD}"
 }
+
